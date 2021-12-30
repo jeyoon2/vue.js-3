247fbf85693feefd57ece95b16196f5c759ecd41
may <may@newming.co.kr>
2021-12-30 21:28:23 +0900

    s1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
-  <div class="name">{{ greet }}</div>
+  <div class="name">{{ name }}</div>
+  <button class="btn btn-primary" v-on:click="updateName">Click</button>
 </template>
 
 <script>
+// import {ref} from "vue" // 화면 동시에 업데이트. 문자열 일때
+import { reactive } from "vue"; // 배열이나 객체일 때 주로 사용
+
 export default {
   setup() {
-    const name = "Jeyoon";
+    const name = reactive({ id: 1 });
 
-    const greeting = (name) => {
-      return `Hello, ${name}`;
+    // const greeting = (name) => {
+    //   return `Hello, ${name}`;
+    // };
+
+    // const greet = greeting(name);
+
+    const updateName = () => {
+      // name.value = Jeyoon
+      name.id = 2;
+      console.log(name);
     };
 
-    const greet = greeting(name);
-
     return {
-      greet,
+      name,
+      updateName,
     };
   },
 };
