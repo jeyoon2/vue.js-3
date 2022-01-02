ec0d7780cb4788496094c1cd49a291318f64b1d2
may <may@newming.co.kr>
2022-01-02 14:42:41 +0900

    section 2

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,56 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <form @submit.prevent="onSubmit" class="d-flex">
-      <div class="d-flex">
-        <div class="flex-grow-1 mr-2">
-          <input
-            class="form-control"
-            type="text"
-            v-model="todo"
-            placeholder="Type new to-do"
-          />
-        </div>
-        <div>
-          <button class="btn btn-primary" type="submit">Add</button>
-        </div>
-      </div>
-      <div v-show="hasError" style="color: red">This field cannot be empty</div>
-    </form>
-    <!-- {{ todos }} -->
-    <div v-for="todo in todos" :key="todo.id" class="card mt-2">
-      <div class="card-body p-2">{{ todo.subject }}</div>
-    </div>
+    <TodoSimpleForm @add-todo="addTodo" />
+
+    <div v-if="!todos.length">추가된 Todo가 없습니다</div>
+    <TodoList
+      :todos="todos"
+      @toggle-todo="toggleTodo"
+      @delete-todo="deleteTodo"
+    />
   </div>
 </template>
 
 <script>
 import { ref } from "vue";
+import TodoSimpleForm from "./components/TodoSimpleForm.vue";
+import TodoList from "./components/TodoList.vue";
 
 export default {
+  components: {
+    TodoSimpleForm,
+    TodoList,
+  },
   setup() {
-    const todo = ref("");
-    const todos = ref([
-      { id: 1, subject: "휴대폰 사기" },
-      { id: 2, subject: "장보기" },
-      { id: 3, subject: "vue 강의 듣기" },
-    ]);
+    const todos = ref([]);
 
-    const onSubmit = () => {
-      console.log(todo.value);
-      todos.value.push({ id: Date.now(), subject: todo.value });
+    const addTodo = (todo) => {
+      todos.value.push(todo);
     };
 
-    const hasError = ref(false);
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
+
+    const toggleTodo = (index) => {
+      console.log(index);
+      todos.value[index].completed = !todos.value[index].completed;
+    };
 
     return {
-      todo,
       todos,
-      onSubmit,
-      hasError,
+      addTodo,
+      deleteTodo,
+      toggleTodo,
     };
   },
 };
 </script>
 
 <style>
-.name {
-  color: green;
+.todo {
+  color: gray;
+  text-decoration: line-through;
 }
 </style>
