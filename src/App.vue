83ac61129e64c2cd460cb58cf1655907d7ef3153
may <may@newming.co.kr>
2022-01-02 15:02:43 +0900

    24

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
+    <input
+      class="form-control"
+      type="text"
+      v-model="searchText"
+      placeholder="Search"
+    />
+    <hr />
     <TodoSimpleForm @add-todo="addTodo" />
 
-    <div v-if="!todos.length">추가된 Todo가 없습니다</div>
+    <div v-if="!filteredTodos.length">There is nothing to display</div>
     <TodoList
-      :todos="todos"
+      :todos="filteredTodos"
       @toggle-todo="toggleTodo"
       @delete-todo="deleteTodo"
     />
@@ -13,7 +20,7 @@
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import TodoSimpleForm from "./components/TodoSimpleForm.vue";
 import TodoList from "./components/TodoList.vue";
 
@@ -38,11 +45,23 @@ export default {
       todos.value[index].completed = !todos.value[index].completed;
     };
 
+    const searchText = ref("");
+    const filteredTodos = computed(() => {
+      if (searchText.value) {
+        return todos.value.filter((todo) => {
+          return todo.subject.includes(searchText.value);
+        });
+      }
+      return todos.value;
+    });
+
     return {
       todos,
       addTodo,
       deleteTodo,
       toggleTodo,
+      searchText,
+      filteredTodos,
     };
   },
 };
