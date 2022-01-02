3a9ada976480fb2f4bb834c8ba5368b0cded7f7b
may <wpdbs4419@gmail.com>
2022-01-02 21:49:23 +0900

    38

--- src/App.vue
+++ src/App.vue
@@ -88,7 +88,7 @@ export default {
 
       try {
         const res = await axios.get(
-          `http://localhost:3000/todos?subject_like=${searchText.value}&_page=${page}&_limit=${limit}`
+          `http://localhost:3000/todos?_sort=id&_order=desc&subject_like=${searchText.value}&_page=${page}&_limit=${limit}`
         );
         numberOfTodos.value = res.headers["x-total-count"];
         todos.value = res.data;
@@ -104,11 +104,11 @@ export default {
       // 데이터베이스에 투두를 저장
       error.value = "";
       try {
-        const res = await axios.post("http://localhost:3000/todos", {
+        await axios.post("http://localhost:3000/todos", {
           subject: todo.subject,
           completed: todo.completed,
         });
-        todos.value.push(res.data);
+        getTodos(1);
       } catch (err) {
         console.log(err);
         error.value = "Something went wrong.";
@@ -124,7 +124,7 @@ export default {
       try {
         await axios.delete("http://localhost:3000/todos/" + id);
 
-        todos.value.splice(index, 1);
+        getTodos(1);
       } catch (err) {
         console.log(err);
         error.value = "Something went wrong.";
